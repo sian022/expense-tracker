c4e07269462d8958b3215e0388c1620858ba3662
sian022 <delacruzsian22@gmail.com>
2024-07-06 14:34:52 +0800

    fix: wrapped the app in ToastProvider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,12 +4,15 @@ import store from "./app/store";
 import { ThemeProvider } from "@mui/material";
 import { RouterProvider } from "react-router-dom";
 import { Provider as ReduxProvider } from "react-redux";
+import { ToastProvider } from "./context/ToastContext";
 
 function App() {
   return (
     <ReduxProvider store={store}>
       <ThemeProvider theme={theme}>
-        <RouterProvider router={router} />
+        <ToastProvider>
+          <RouterProvider router={router} />
+        </ToastProvider>
       </ThemeProvider>
     </ReduxProvider>
   );
