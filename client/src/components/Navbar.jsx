bc6dccf1ea8ff48486c6cc96fbf62cadafd6f684
sian022 <delacruzsian22@gmail.com>
2024-07-06 10:44:19 +0800

    feat: finished navbar (added currency selector)

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,15 @@
-import { Stack, Typography } from "@mui/material";
+import { MenuItem, Stack, TextField, Typography } from "@mui/material";
+import logo from "../assets/images/logo-no-bg.png";
+import { useNavigate } from "react-router-dom";
+import currencies from "../utils/currencies";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+
+  const handleLogoClick = () => {
+    navigate("/");
+  };
+
   return (
     <Stack
       direction="row"
@@ -9,15 +18,43 @@ const Navbar = () => {
       top={0}
       alignItems="center"
       justifyContent="space-between"
+      px={3}
+      py={2}
     >
-      <Typography
-        variant="h4"
-        fontFamily="Montserrat"
-        fontWeight="700"
-        color="primary.main"
+      <Stack
+        direction="row"
+        alignItems="center"
+        spacing={2}
+        sx={{ cursor: "pointer" }}
+        onClick={handleLogoClick}
       >
-        Navbar
-      </Typography>
+        <img src={logo} alt="logo" width="50" />
+
+        <Typography
+          variant="h4"
+          fontFamily="Montserrat"
+          fontWeight="700"
+          color="primary.main"
+        >
+          Expense Tracker
+        </Typography>
+      </Stack>
+
+      <TextField
+        select
+        size="small"
+        sx={{ width: "140px" }}
+        placeholder="Currency"
+      >
+        {currencies.map((c) => (
+          <MenuItem key={c.currency} value={c.currency}>
+            <Stack direction="row" spacing={2} alignItems="center">
+              <img src={c.flagSrc} width="20px" alt={c.name} />
+              <Typography>{c.currency}</Typography>
+            </Stack>
+          </MenuItem>
+        ))}
+      </TextField>
     </Stack>
   );
 };
---
+++ client/src/utils/currencies.js
@@ -0,0 +1,26 @@
+import USAFlag from "../assets/images/usa.png";
+import PhilippinesFlag from "../assets/images/philippines.png";
+import CanadaFlag from "../assets/images/canada.png";
+
+const currencies = [
+  {
+    country: "USA",
+    currency: "USD",
+    flagSrc: USAFlag,
+    conversionRate: 1,
+  },
+  {
+    country: "Philippines",
+    currency: "PHP",
+    flagSrc: PhilippinesFlag,
+    conversionRate: 58.52,
+  },
+  {
+    country: "Canada",
+    currency: "CAD",
+    flagSrc: CanadaFlag,
+    conversionRate: 1.36,
+  },
+];
+
+export default currencies;
